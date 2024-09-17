1a8a8945b913a2bc1222952d3552adc4f08a21a3
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-17 21:05:12 +0100

    hehe

--- src/main/python/widgets/square_button.py
+++ src/main/python/widgets/square_button.py
@@ -1,45 +1,68 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QFont, QFontMetrics
 from PyQt5.QtWidgets import QPushButton, QLabel, QHBoxLayout
 
 class SquareButton(QPushButton):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-
         self.scale = 1.2
         self.label = None
         self.word_wrap = True
         self.text = ""
+        self.adjust_font_size = True
 
     def setRelSize(self, ratio):
         self.scale = ratio
         self.updateGeometry()
+        self.update_text()
 
     def setWordWrap(self, state):
         self.word_wrap = state
-        self.setText(self.text)
+        self.update_text()
 
     def sizeHint(self):
         size = int(round(self.fontMetrics().height() * self.scale))
         return QSize(size, size)
 
-    # Override setText to facilitate automatic word wrapping
     def setText(self, text):
         self.text = text
+        self.update_text()
+
+    def update_text(self):
         if self.word_wrap:
-            super().setText("")
             if self.label is None:
-                self.label = QLabel(text, self)
+                self.label = QLabel(self)
                 self.label.setWordWrap(True)
                 self.label.setAlignment(Qt.AlignCenter)
                 layout = QHBoxLayout(self)
                 layout.setContentsMargins(0, 0, 0, 0)
-                layout.addWidget(self.label,0,Qt.AlignCenter)
-            else:
-                self.label.setText(text)
+                layout.addWidget(self.label, 0, Qt.AlignCenter)
+            self.adjust_font_size_if_needed()
+            self.label.setText(self.text)
         else:
             if self.label is not None:
                 self.label.deleteLater()
-            super().setText(text)
+            super().setText(self.text)
+
+    def adjust_font_size_if_needed(self):
+        if self.adjust_font_size:
+            font = self.font()
+            font_size = font.pointSize()
+            fm = QFontMetrics(font)
+            text_width = fm.width(self.text)
+            text_height = fm.height()
+            button_width = self.width()
+            button_height = self.height()
+
+            while text_width > button_width or text_height > button_height:
+                font_size -= 1
+                font.setPointSize(font_size)
+                fm = QFontMetrics(font)
+                text_width = fm.width(self.text)
+                text_height = fm.height()
+                if font_size <= 1:  # Avoid too small font sizes
+                    break
+            self.label.setFont(font)
