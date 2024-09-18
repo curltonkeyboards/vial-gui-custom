e09bfcc74b2ba21d24ad3f3dbecbcf122f4a6e62
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-18 02:19:47 +0100

    Update keyboard_widget.py

--- src/main/python/widgets/keyboard_widget.py
+++ src/main/python/widgets/keyboard_widget.py
@@ -477,7 +477,7 @@ class KeyboardWidget(QWidget):
 
     def hit_test(self, pos):
         """ Returns key, hit_masked_part """
-        scaled_pos = pos / self.scale
+        scaled_pos = pos / (self.scale * 1.3)
         for key in self.widgets:
             if key.masked and key.mask_polygon.containsPoint(pos/self.scale, Qt.OddEvenFill):
                 return key, True
