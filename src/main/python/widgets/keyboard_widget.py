9d0d89795d4d67231677fcd1b6ef5f42aa7a713c
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-18 01:55:14 +0100

    Update keyboard_widget.py

--- src/main/python/widgets/keyboard_widget.py
+++ src/main/python/widgets/keyboard_widget.py
@@ -350,8 +350,8 @@ class KeyboardWidget(QWidget):
         max_w = max_h = 0
         for key in self.widgets:
             p = key.polygon.boundingRect().bottomRight()
-            max_w = max(max_w, p.x() * (self.scale * 1.3, self.scale * 1.3))
-            max_h = max(max_h, p.y() * (self.scale * 1.3, self.scale * 1.3))
+            max_w = max(max_w, p.x() * (self.scale * 1.5))
+            max_h = max(max_h, p.y() * (self.scale * 1.5))
 
         self.width = round(max_w + 2 * self.padding)
         self.height = round(max_h + 2 * self.padding)
@@ -415,7 +415,7 @@ class KeyboardWidget(QWidget):
         for idx, key in enumerate(self.widgets):
             qp.save()
             #responsible for scaling everything including encoders etc. does not scale the hitbox of the button however
-            qp.scale(self.scale * 1.3, self.scale * 1.3)        
+            qp.scale(self.scale, self.scale)         
             qp.translate(key.shift_x, key.shift_y)
             qp.translate(key.rotation_x, key.rotation_y)
             qp.rotate(key.rotation_angle)
