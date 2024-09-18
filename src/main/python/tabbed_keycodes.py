d91207a3a320c47ade9cee5055301dc346df4988
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-18 23:08:56 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -274,7 +274,7 @@ class FilteredTabbedKeycodes(QTabWidget):
             SimpleTab(self, "MIDI Notes", KEYCODES_MIDI),
             Tab(self, "MIDI", [
                 (midi_layout, KEYCODES_MIDI_CHANNEL),                
-            ], prefix_buttons=[("Any", -1)]),
+            ], prefix_buttons=[()]),
             SmartChordTab(self, "SmartChord", KEYCODES_MIDI_CHORD, KEYCODES_MIDI_SCALES, KEYCODES_MIDI_INVERSION),   # Updated to SmartChordTab
             SimpleTab(self, "MIDI Channel", KEYCODES_MIDI_CHANNEL),
             SimpleTab(self, "MIDI Transpose", KEYCODES_MIDI_TRANSPOSITION),
