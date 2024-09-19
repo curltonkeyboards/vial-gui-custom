319faed532e3af2a126f00c616a5a4a8fae967d9
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-19 22:45:51 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -372,10 +372,9 @@ class SmartChordTab(QWidget):
                     if readable_name in ["C#\nDb", "C#3\nDb3"]:
                         button.setStyleSheet("background-color: rgba(30, 30, 30, 1); color: rgba(190, 190, 190, 1);")
                         
-                        
                     if readable_name in ["C#1\nDb1", "C#2\nDb2", "C#4\nDb4", "C#5\nDb5"]:
                         button.setStyleSheet("background-color: rgba(30, 30, 30, 1); color: rgba(190, 190, 190, 1);")
-                        hbox.addSpacing(50)
+                        hbox.addSpacing(70)                      
                         
                     if readable_name in ["F#\nGb", "F#1\nGb1", "F#2\nGb2", "F#3\nGb3", "F#4\nGb4", "F#5\nGb5"]:
                         button.setStyleSheet("background-color: rgba(30, 30, 30, 1); color: rgba(190, 190, 190, 1);")
