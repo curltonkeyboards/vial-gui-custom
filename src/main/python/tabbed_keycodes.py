8255f2d809c395ffdfbd3e0b29d49ca7a82a6dbd
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-19 20:14:53 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -207,17 +207,17 @@ class SmartChordTab(QWidget):
 
         # Define MIDI layout
         self.midi_layout2 = [
-            ["MI_Cs", "MI_Ds", {"x": 1}, "MI_Fs", "MI_Gs", "MI_As",
-             {"x": 1}, "MI_Cs_1", "MI_Ds_1", {"x": 1}, "MI_Fs_1", "MI_Gs_1", "MI_As_1",
-             {"x": 1}, "MI_Cs_2", "MI_Ds_2", {"x": 1}, "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
+            ["MI_Cs", "MI_Ds", "MI_Fs", "MI_Gs", "MI_As",
+            "MI_Cs_1", "MI_Ds_1", "MI_Fs_1", "MI_Gs_1", "MI_As_1",
+            "MI_Cs_2", "MI_Ds_2", "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
 
             ["MI_C", "MI_D", "MI_E", "MI_F", "MI_G", "MI_A", "MI_B",
              "MI_C_1", "MI_D_1", "MI_E_1", "MI_F_1", "MI_G_1", "MI_A_1", "MI_B_1",
              "MI_C_2", "MI_D_2", "MI_E_2", "MI_F_2", "MI_G_2", "MI_A_2", "MI_B_2"],
 
-            ["MI_Cs_3", "MI_Ds_3", {"x": 1}, "MI_Fs_3", "MI_Gs_3", "MI_As_3",
-             {"x": 1}, "MI_Cs_4", "MI_Ds_4", {"x": 1}, "MI_Fs_4", "MI_Gs_4", "MI_As_4",
-             {"x": 1}, "MI_Cs_5", "MI_Ds_5", {"x": 1}, "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
+            ["MI_Cs_3", "MI_Ds_3", "MI_Fs_3", "MI_Gs_3", "MI_As_3",
+             "MI_Cs_4", "MI_Ds_4", "MI_Fs_4", "MI_Gs_4", "MI_As_4",
+             "MI_Cs_5", "MI_Ds_5", "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
 
             ["MI_C_3", "MI_D_3", "MI_E_3", "MI_F_3", "MI_G_3", "MI_A_3", "MI_B_3",
              "MI_C_4", "MI_D_4", "MI_E_4", "MI_F_4", "MI_G_4", "MI_A_4", "MI_B_4",
@@ -364,7 +364,12 @@ class SmartChordTab(QWidget):
                     if "#" in readable_name:  # Sharp keys have # in their name
                         button.setStyleSheet("background-color: rgba(30, 30, 30, 1); color: rgba(190, 190, 190, 1);")
                         # Add an empty space before the black keys to stagger
-                        hbox.addSpacing(15)
+                        
+                    if readable_name in ["C#\nDb", "C#3\nDb3"]:
+                        hbox.addSpacing(20)
+                        
+                    if readable_name in ["C#1\nDb1", "C#2\nDb2", "C#4\nDb4", "C#5\nDb5", "F#\nGb", "F#1\nGb1", "F#2\nGb2", "F#3\nGb3", "F#4\nGb4", "F#5\nGb5"]:
+                        hbox.addSpacing(40)
 
                     else:
                         button.setStyleSheet("background-color: rgba(190, 190, 190, 1); color: rgba(30, 30, 30, 1);")
