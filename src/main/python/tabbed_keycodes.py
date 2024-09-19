7e110ef84288c24cbdf23c08e148f67fe91c8599
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-19 13:50:02 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -207,17 +207,17 @@ class SmartChordTab(QWidget):
 
         # Define MIDI layout
         self.midi_layout2 = [
-            [{"x": 500}, "MI_Cs", "MI_Ds", "MI_Fs", "MI_Gs", "MI_As",
-             "MI_Cs_1", "MI_Ds_1", "MI_Fs_1", "MI_Gs_1", "MI_As_1",
-             "MI_Cs_2", "MI_Ds_2", "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
+            ["MI_Cs", "MI_Ds", {"x": 1}, "MI_Fs", "MI_Gs", "MI_As",
+             {"x": 1}, "MI_Cs_1", "MI_Ds_1", {"x": 1}, "MI_Fs_1", "MI_Gs_1", "MI_As_1",
+             {"x": 1}, "MI_Cs_2", "MI_Ds_2", {"x": 1}, "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
 
             ["MI_C", "MI_D", "MI_E", "MI_F", "MI_G", "MI_A", "MI_B",
              "MI_C_1", "MI_D_1", "MI_E_1", "MI_F_1", "MI_G_1", "MI_A_1", "MI_B_1",
              "MI_C_2", "MI_D_2", "MI_E_2", "MI_F_2", "MI_G_2", "MI_A_2", "MI_B_2"],
 
-            ["MI_Cs_3", "MI_Ds_3", "MI_Fs_3", "MI_Gs_3", "MI_As_3",
-             "MI_Cs_4", "MI_Ds_4", "MI_Fs_4", "MI_Gs_4", "MI_As_4",
-             "MI_Cs_5", "MI_Ds_5", "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
+            ["MI_Cs_3", "MI_Ds_3", {"x": 1}, "MI_Fs_3", "MI_Gs_3", "MI_As_3",
+             {"x": 1}, "MI_Cs_4", "MI_Ds_4", {"x": 1}, "MI_Fs_4", "MI_Gs_4", "MI_As_4",
+             {"x": 1}, "MI_Cs_5", "MI_Ds_5", {"x": 1}, "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
 
             ["MI_C_3", "MI_D_3", "MI_E_3", "MI_F_3", "MI_G_3", "MI_A_3", "MI_B_3",
              "MI_C_4", "MI_D_4", "MI_E_4", "MI_F_4", "MI_G_4", "MI_A_4", "MI_B_4",
@@ -357,17 +357,14 @@ class SmartChordTab(QWidget):
             for col_index, item in enumerate(row):
                 if isinstance(item, str):
                     readable_name = name_mapping.get(item, item)
-                    button = SquareButton()
+                    button = BigSquareButton()
                     button.setText(readable_name)
                     if "#" in readable_name:  # Sharp keys have # in their name
                         button.setStyleSheet("background-color: rgba(70, 70, 70, 1); color: rgba(145, 145, 145, 1);")
                     else:
                         button.setStyleSheet("background-color: rgba(145, 145, 145, 1); color: rgba(70, 70, 70, 1);")
-                    if "x" in item:
-                        spacer_width = item.get("x", 0)  # Value in pixels, instead of multiplier
-                        spacer = QSpacerItem(spacer_width, 0, QSizePolicy.Fixed, QSizePolicy.Minimum)
-                        container_layout.addItem(spacer, row_index, col_index)
-                    button.setFixedHeight(30)  # Set size as needed
+                    
+                    button.setFixedSize(30, 30)  # Set size as needed
                     button.clicked.connect(lambda _, text=item: self.keycode_changed.emit(text))
                     container_layout.addWidget(button, row_index, col_index)               
 
