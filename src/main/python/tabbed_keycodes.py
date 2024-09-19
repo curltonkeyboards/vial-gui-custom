738fef39690460f5b058f24ce500dffea7b9d986
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-19 04:34:18 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -292,20 +292,20 @@ class SmartChordTab(QWidget):
 
         # Define MIDI layout
         self.midi_layout2 = [
-            [{"z": 0}, "MI_Cs", "MI_Ds", {"x": 0}, "MI_Fs", "MI_Gs", "MI_As", {"x": 0}, {"x": 0},
-             "MI_Cs_1", "MI_Ds_1", {"x": 0}, "MI_Fs_1", "MI_Gs_1", "MI_As_1", {"x": 0}, {"x": 0},
-             "MI_Cs_2", "MI_Ds_2", {"x": 0}, "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
+            [{"x": 0.5}, "MI_Cs", "MI_Ds", {"x": 1}, "MI_Fs", "MI_Gs", "MI_As", {"x": 2},
+             "MI_Cs_1", "MI_Ds_1", {"x": 1}, "MI_Fs_1", "MI_Gs_1", "MI_As_1", {"x": 2},
+             "MI_Cs_2", "MI_Ds_2", {"x": 1}, "MI_Fs_2", "MI_Gs_2", "MI_As_2"],
 
-            ["MI_C", "MI_D", "MI_E", "MI_F", "MI_G", "MI_A", "MI_B", {"x": 0},
-             "MI_C_1", "MI_D_1", "MI_E_1", "MI_F_1", "MI_G_1", "MI_A_1", "MI_B_1", {"x": 0},
+            [{"y": 0}, {"y": 0.5}, {"x": 0}, "MI_C", "MI_D", "MI_E", "MI_F", "MI_G", "MI_A", "MI_B", {"x": 1.0},
+             "MI_C_1", "MI_D_1", "MI_E_1", "MI_F_1", "MI_G_1", "MI_A_1", "MI_B_1", {"x": 1.0},
              "MI_C_2", "MI_D_2", "MI_E_2", "MI_F_2", "MI_G_2", "MI_A_2", "MI_B_2"],
 
-            [{"z": 0}, "MI_Cs_3", "MI_Ds_3", {"x": 0}, "MI_Fs_3", "MI_Gs_3", "MI_As_3", {"x": 0}, {"x": 0},
-             "MI_Cs_4", "MI_Ds_4", {"x": 0}, "MI_Fs_4", "MI_Gs_4", "MI_As_4", {"x": 0}, {"x": 0},
-             "MI_Cs_5", "MI_Ds_5", {"x": 0}, "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
+            [{"y": 0}, {"y": 1}, {"x": 0}, {"x": 0.5}, "MI_Cs_3", "MI_Ds_3", {"x": 1}, "MI_Fs_3", "MI_Gs_3", "MI_As_3", {"x": 2},
+             "MI_Cs_4", "MI_Ds_4", {"x": 1}, "MI_Fs_4", "MI_Gs_4", "MI_As_4", {"x": 2},
+             "MI_Cs_5", "MI_Ds_5", {"x": 1}, "MI_Fs_5", "MI_Gs_5", "MI_As_5"],
 
-            ["MI_C_3", "MI_D_3", "MI_E_3", "MI_F_3", "MI_G_3", "MI_A_3", "MI_B_3", {"x": 0},
-             "MI_C_4", "MI_D_4", "MI_E_4", "MI_F_4", "MI_G_4", "MI_A_4", "MI_B_4", {"x": 0},
+            [{"y": 0}, {"y": 0.5}, {"x": 0}, "MI_C_3", "MI_D_3", "MI_E_3", "MI_F_3", "MI_G_3", "MI_A_3", "MI_B_3", {"x": 1},
+             "MI_C_4", "MI_D_4", "MI_E_4", "MI_F_4", "MI_G_4", "MI_A_4", "MI_B_4", {"x": 1},
              "MI_C_5", "MI_D_5", "MI_E_5", "MI_F_5", "MI_G_5", "MI_A_5", "MI_B_5"]
         ]
 
@@ -449,11 +449,6 @@ class SmartChordTab(QWidget):
                     if "x" in item:
                         x = item.get("x", 0)
                         spacer_width = int(x * 30)  # Adjust width as needed
-                        spacer = QSpacerItem(spacer_width, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
-                        container_layout.addItem(spacer, row_index, col_index)
-                        if "z" in item:
-                        x = item.get("z", 0)
-                        spacer_width = int(x * 15)  # Adjust width as needed
                         spacer = QSpacerItem(spacer_width, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
                         container_layout.addItem(spacer, row_index, col_index)
                     if "y" in item:
