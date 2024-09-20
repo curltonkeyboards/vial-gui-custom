8895d0a087d2c836e58ad06f4e964f3e81132c13
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-09-20 15:54:58 +0100

    Update tabbed_keycodes.py

--- src/main/python/tabbed_keycodes.py
+++ src/main/python/tabbed_keycodes.py
@@ -2,7 +2,7 @@
 
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtWidgets import QTabWidget, QWidget, QScrollArea, QApplication, QVBoxLayout, QComboBox, QSizePolicy, QLabel, QGridLayout
-from PyQt5.QtGui import QPalette
+from PyQt5.QtGui import QPalette, QPainter, QStyleOptionComboBox
 
 from constants import KEYCODE_BTN_RATIO
 from widgets.display_keyboard import DisplayKeyboard
