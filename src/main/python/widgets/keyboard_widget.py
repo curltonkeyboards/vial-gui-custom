e07b759e9a0883f440fd9862e609a3435fb2c001
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-10-30 17:09:45 +0000

    Update keyboard_widget.py

--- src/main/python/widgets/keyboard_widget.py
+++ src/main/python/widgets/keyboard_widget.py
@@ -258,7 +258,7 @@ class KeyboardWidget(QWidget):
         super().__init__()
 
         self.enabled = True
-        self.scale = 0.5
+        self.scale = 0.8
         self.padding = KEYBOARD_WIDGET_PADDING
 
         self.setMouseTracking(True)
@@ -350,11 +350,11 @@ class KeyboardWidget(QWidget):
         max_w = max_h = 0
         for key in self.widgets:
             p = key.polygon.boundingRect().bottomRight()
-            max_w = max(max_w, p.x() * (self.scale * 0.5))
-            max_h = max(max_h, p.y() * (self.scale * 0.5))
+            max_w = max(max_w, p.x() * (self.scale * 1.3))
+            max_h = max(max_h, p.y() * (self.scale * 1.3))
 
-        self.width = round(max_w + 1 * self.padding)
-        self.height = round(max_h + 1 * self.padding)
+        self.width = round(max_w + 2 * self.padding)
+        self.height = round(max_h + 2 * self.padding)
 
         self.update()
         self.updateGeometry()
