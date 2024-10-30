682bbb1fb7031fce5a71aa18c4a046bf65680eab
curltonkeyboards <151652929+curltonkeyboards@users.noreply.github.com>
2024-10-30 17:04:50 +0000

    Update keyboard_widget.py

--- src/main/python/widgets/keyboard_widget.py
+++ src/main/python/widgets/keyboard_widget.py
@@ -350,8 +350,8 @@ class KeyboardWidget(QWidget):
         max_w = max_h = 0
         for key in self.widgets:
             p = key.polygon.boundingRect().bottomRight()
-            max_w = max(max_w, p.x() * (self.scale * 1.3))
-            max_h = max(max_h, p.y() * (self.scale * 1.3))
+            max_w = max(max_w, p.x() * (self.scale * 0.5))
+            max_h = max(max_h, p.y() * (self.scale * 0.5))
 
         self.width = round(max_w + 2 * self.padding)
         self.height = round(max_h + 2 * self.padding)
